b75f4e18bc845a0ce5de7a59d6c6eeab64d6e516
chitra-git-hub <chitralakshmanan1@gmail.com>
2025-06-21 20:39:22 +0530

    Add files via upload
    Day 1 Assignments

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println(
+				"Browser Version:" + version + "\n" + "Developer:" + developer + "\n" + "Is Beta:"
+						+ isBeta + "\n" + "Release year:" + releaseYear + "\n" + "ShortCutKey:" + shortcutKey);
+	}
+
+}
---
+++ FireFox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println(
+				"Browser Version:" + browserVersion + "\n" + "Browser Name:" + browserName + "\n" + "Is Visible:"
+						+ isVisible + "\n" + "Release year:" + releaseYear + "\n" + "Browserlogo:" + browserLogo);
+	}
+
+}
